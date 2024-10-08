7c27c373f054c3f628afa0a3f29e46e95f8e58ce
Tarun kumar <115946244+krtarun7@users.noreply.github.com>
2024-10-08 03:27:29 +0000

    Method program

---
+++ findsmallest.java
@@ -0,0 +1,19 @@
+public class findsmallest {
+    static void Smallestfind(int a, int b , int c){
+        if(a < b && a < c){
+            System.out.println("Smallest number is " + a);
+        }
+        else {
+            if(b < a && b < c){
+                System.out.println("Smallest number is " + b);
+            }
+            else{
+                System.out.println("Smallest number is " + c);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Smallestfind(25,37,29);
+}
+}
+
