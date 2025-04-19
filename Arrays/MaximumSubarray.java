f20a86c4d492662a98c60218e862c1a8958be2a6
krtarun7 <tarun7011kumar@gmail.com>
2025-04-19 13:13:10 +0530

    Using Kadane's Algorithm

--- Arrays/MaximumSubarray.java
+++ Arrays/MaximumSubarray.java
@@ -19,12 +19,28 @@ public class MaximumSubarray {
                 sum = 0;
             }
         }
-        return max;
+        return sum;
+        /*
+        if we print the subarray with maximum subarray sum
+        then we will replace the retrun statement with this code
+        */
+
+        /*
+        System.out.println("Maximum Subarray Sum: " + max);
+        System.out.print("Subarray: ");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        */
     }
 
 
     public static void main(String[] args) {
         int[] arr={-2,-3,4,-1,-2,1,5,-3};
         System.out.println(MaxSubarray(arr));
+        /*here replace the print statement with call the function like MaximumSubarray(arr);
+        and change the type of function from int to void */
     }
 }
