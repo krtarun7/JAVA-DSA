ca87fbc3791056d3460d5b58f10eb895758cee83
krtarun7 <tarun7011kumar@gmail.com>
2025-04-23 15:21:29 +0530

    Find the lonegst Consecutive Sequence in an Array

---
+++ Arrays/LongestConsecutiveNumbers.java
@@ -0,0 +1,38 @@
+package Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveNumbers {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            // only start counting if num is the start of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = longestConsecutive(nums);
+        System.out.println("Longest consecutive sequence length: " + result);
+    }
+}
